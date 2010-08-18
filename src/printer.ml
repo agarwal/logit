61a222efed0f6cbf414c5c13c72c0f33b0468e1f
Ashish Agarwal <agarwal1975@gmail.com>
2010-08-18 00:36:54 -0400

    Implemented Printer.print_string, completing main functionality. Just need a main driver now.

--- src/printer.ml
+++ src/printer.ml
@@ -12,6 +12,12 @@ type num_modifier =
     | Minus of int (** [Minus n] subtracts [n] from a given number *)
     | NumIdentity (** return given number unmodified *)
 
+let apply_num_modifier (f : num_modifier) (x : int) : int =
+  match f with
+    | Plus y -> x + y
+    | Minus y -> x - y
+    | NumIdentity -> x
+
 (** Types of numeric format specifiers. *)
 type num_spec = YearSpec | MonthSpec | DaySpec | HourSpec | MinuteSpec | SecondSpec
 
@@ -21,7 +27,7 @@ type spec =
     | CharSpec of char
     | StringSpec of index
     | NumSpec of num_modifier * num_spec
-        
+
 (** Type of a printer, i.e. a specification of how to print an
     output file name. Also called a format string. *)
 type t = spec list
@@ -59,20 +65,24 @@ let to_string (t:t) : string =
 
   String.concat "" (List.map spec_to_string t)
 
-(*
-
 (** [print_string t data] returns a string formatted according to
-  printer [t] and using given [data]. *)
-let print_string printer data =
+    printer [t] and using given [data]. *)
+let print_string (printer : t) (data : Data.t) : string =
   let rec loop ans printer =
     match printer with
       | [] -> ans
       | (CharSpec c)::printer -> loop (sprintf "%s%c" ans c) printer
-      | StringSpec::printer -> loop (sprintf "%s%s" ans (Data.string dat)) printer
-      | DefaultNumSpec YearSpec ->
-          loop (sprintf "%s%d" ans (Data.year dat)) printer
-      | DefaultNumSpec MonthSpec ->
-          loop (sprintf "%s%d" ans (Data.month dat)) printer
-      | ExactNumSpec (n,YearSpec) ->
-          loop 
-*)
+      | (StringSpec n)::printer -> loop (sprintf "%s%s" ans (Data.string n data)) printer
+      | (NumSpec (f,x))::printer ->
+          let g = match x with
+            | YearSpec -> Data.year
+            | MonthSpec -> Data.month
+            | DaySpec -> Data.day
+            | HourSpec -> Data.hour
+            | MinuteSpec -> Data.minute
+            | SecondSpec -> Data.second
+          in
+          let y = apply_num_modifier f (g data) in
+          loop (sprintf "%s%d" ans y) printer
+  in
+  loop "" printer
